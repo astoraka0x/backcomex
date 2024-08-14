d462d2efb389ce409d3879f53c09573ca1c051f9
astoraka0x <astoraka9@gmaill.com>
2024-08-14 12:04:20 +0700

    Update Banner component to use translated text for "how-it-work" button

--- app/[locale]/components/Banner/index.tsx
+++ app/[locale]/components/Banner/index.tsx
@@ -28,7 +28,7 @@ const Banner = () => {
                             <p className='text-white md:text-lg font-normal mb-10 md:text-start text-center'> {t('Banner_Title')}</p>
                             <div className='flex align-middle justify-center md:justify-start'>
                                 <button className='text-xl font-semibold text-white py-4 px-6 lg:px-12 navbutton mr-6'>{t('Get_Started')}</button>
-                                <button onClick={() => setOpen(true)} className='bg-transparent flex justify-center items-center text-white'><Image src={'/images/Banner/playbutton.svg'} alt="button-image" className='mr-3' width={47} height={47} />How it work</button>
+                                <button onClick={() => setOpen(true)} className='bg-transparent flex justify-center items-center text-white'><Image src={'/images/Banner/playbutton.svg'} alt="button-image" className='mr-3' width={47} height={47} />{t('how-it-work')}</button>
                             </div>
                         </div>
 
